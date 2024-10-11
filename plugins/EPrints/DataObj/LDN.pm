b1f9981794f27e85ed118828125d4b28480a5556
Will Fyson <will.fyson@cosector.com>
2024-10-11 13:30:56 +0000

    More of the LDN skeleton...

--- plugins/EPrints/DataObj/LDN.pm
+++ plugins/EPrints/DataObj/LDN.pm
@@ -53,9 +53,26 @@ sub get_defaults
 
     # UUID - TODO
 
-    # Timestamp
-    $data->{timestamp} = EPrints::Time::get_iso_timestamp();
-
     return $data;
 }
 
+sub send_ldn
+{
+    # TODO: Looks up the inbox for this LDN's "to" value
+    #       Makes a POST to the given inbox using Content Type "application/ld+json"
+    #       Set the timestamp on the LDN record after POST-ing
+}
+
+sub build_payload
+{
+    my( $class, $session, $origin ) = @_;
+
+    # TODO: Builds a JSON payload including...
+    #       @context: Defaults to "https://www.w3.org/ns/activitystreams" and "http://purl.org/coar/notify"
+    #       origin: This will (always?) default to us! Need to provide type, id and inbox
+    #       actor: Again this will default to us, but this time we provide a name from than an inbox
+    #       id: Uuid which we should already have in the LDN object
+    #       object: Convert the given dataobj into an LDN object - a function for each invidual dataobj, but almost certainly just an eprint for now. Needs a type, id and cite-as (see signposting)
+    #       target: another LDN inbox - get details from our LDN inbox dataset
+    #       type: the kind of LDN we're sending
+}
